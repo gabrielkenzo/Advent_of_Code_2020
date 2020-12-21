7503d6bcd17e2365dd94bae29659e4fb9c4e4b4a
Gabriel Kenzo Sumida <kenzogabriels@hotmail.com>
2020-12-21 01:48:10 -0300

    day 2

--- day2/ex1.py
+++ day2/ex1.py
@@ -1,13 +1,16 @@
 import os
 import sys
 
-file = open(os.path.join(sys.path[0], "input.txt"))
-lines = file.readlines()
-count = 0
-for k in range(len(lines)):
-    line = lines[k].split()
-    occurrences = line[2].count(line[1][0])
-    bounds = line[0].split("-")
-    if occurrences in range(int(bounds[0]), int(bounds[1]) + 1):
-        count += 1
-print(count)
+if __name__ == '__main__':
+    file = open(os.path.join(sys.path[0], "input.txt"))
+    lines = file.readlines()
+    file.close()
+    
+    count = 0
+    for k in range(len(lines)):
+        line = lines[k].split()
+        occurrences = line[2].count(line[1][0])
+        bounds = line[0].split("-")
+        if occurrences in range(int(bounds[0]), int(bounds[1]) + 1):
+            count += 1
+    print(count)
--- day2/ex2.py
+++ day2/ex2.py
@@ -1,16 +1,19 @@
 import os
 import sys
 
-file = open(os.path.join(sys.path[0], "input.txt"))
-lines = file.readlines()
-count = 0
-for k in range(len(lines)):
-    line = lines[k].split()
-    letter = line[1][0]
-    occurrences = line[2].count(letter)
-    index = line[0].split("-")
+if __name__ == '__main__':
+    file = open(os.path.join(sys.path[0], "input.txt"))
+    lines = file.readlines()
+    file.close()
+    
+    count = 0
+    for k in range(len(lines)):
+        line = lines[k].split()
+        letter = line[1][0]
+        occurrences = line[2].count(letter)
+        index = line[0].split("-")
 
-    #XOR condition
-    if (line[2][int(index[0]) - 1] == letter) is not (line[2][int(index[1]) - 1] == letter):
-        count += 1
-print(count)
+        #XOR condition
+        if (line[2][int(index[0]) - 1] == letter) is not (line[2][int(index[1]) - 1] == letter):
+            count += 1
+    print(count)
